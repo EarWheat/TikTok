1674c48b55fbde2af02af29958bffa966afc7458
liuzhaoluliuzhaolu <liuzhaoluliuzhaolu@didiglobal.com>
2021-03-08 17:55:38 +0800

    自动部署sh文件

---
+++ tiktok-back/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export TOMCAT_HOME=/home/liuzhaolu/tomcat/tomcat8/webapps/
+
+dir=`dirname $0`
+path=`cd ${dir} && pwd`
+echo $path
+cd ${path}
+mvn clean package -Dmaven.test.skip=true
+ret=$?
+if [ $ret -ne 0 ];then
+    echo "===== maven build failure ====="
+    exit $ret
+else
+    echo -n "===== maven build successfully! ====="
+fi
+cd ${path}
+rm -rf ${TOMCAT_HOME}/TikTokPlayer-1.0-SNAPSHOT.war
+
+mv ${path}/target/TikTokPlayer-1.0-SNAPSHOT.war ${TOMCAT_HOME}  #拷贝目标war包或者jar包等至output目录下
